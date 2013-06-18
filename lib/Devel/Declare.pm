66ecedc8d6c92f0664635002ef3d55b896cca80b
Karen Etheridge <ether@cpan.org>
2013-06-18 15:54:51 -0700

    bump version for imminent release

--- lib/Devel/Declare.pm
+++ lib/Devel/Declare.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use 5.008001;
 
-our $VERSION = '0.006011';
+our $VERSION = '0.006012';
 
 use constant DECLARE_NAME => 1;
 use constant DECLARE_PROTO => 2;
--- lib/Devel/Declare/MethodInstaller/Simple.pm
+++ lib/Devel/Declare/MethodInstaller/Simple.pm
@@ -7,7 +7,7 @@ use Sub::Name;
 use strict;
 use warnings;
 
-our $VERSION = '0.006011';
+our $VERSION = '0.006012';
 
 sub install_methodhandler {
   my $class = shift;
